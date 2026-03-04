6f03661f2583969fbd5d36772185933d06f98ecf
cadenbents <cbtech4581@gmail.com>
2026-03-04 15:04:36 -0700

    Day 2 work, finished Node class

--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,45 @@
 public class Node<E> {
 
+    private E data;
+    private Node<E> next;
+    private Node<E> previous;
+
+    public Node(E data){
+        this.data = data;
+        this.next = null;
+        this.previous = null;
+    }
+
+    public Node(E data, Node<E> n){
+        this.data = data;
+        this.next = null;
+        this.previous = n;
+    }
+
+
+
+    public void setNextNode(Node<E> n){
+        this.next = n;
+    }
+
+    public void setPrevNode(Node<E> p){
+        this.previous = p;
+    }
+
+    public Node<E> getNextNode(){
+        return next;
+    }
+
+    public Node<E> getPrevNode(){
+        return previous;
+    }
+
+    public E getValue(){
+        return data;
+    }
+
+    public void setValue(E data){
+        this.data = data;
+    }
 }
+
