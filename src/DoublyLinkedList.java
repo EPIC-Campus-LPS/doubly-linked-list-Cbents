7fd3036c92a9063f5ddcee3fddf6da7c902b0e56
cadenbents <cbtech4581@gmail.com>
2026-03-04 15:08:16 -0700

    Day 2 work, finished Node class, add class

---
+++ src/DoublyLinkedList.java
@@ -0,0 +1,42 @@
+public class DoublyLinkedList<E> implements List<E>{
+
+    @Override
+    public void add(E element) {
+
+    }
+
+    @Override
+    public void add(int i, E element) throws IndexOutOfBoundsException {
+
+    }
+
+    @Override
+    public E remove() {
+        return null;
+    }
+
+    @Override
+    public E remove(int i) throws IndexOutOfBoundsException {
+        return null;
+    }
+
+    @Override
+    public Node<E> get(int i) throws IndexOutOfBoundsException {
+        return null;
+    }
+
+    @Override
+    public void set(int i, E element) throws IndexOutOfBoundsException {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
