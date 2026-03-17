39f911f5495ee014e7012446fe4301598845d26a
cadenbents <cbtech4581@gmail.com>
2026-03-17 15:07:09 -0600

    Day 3 work, started Doubly linked list, left off at add (int i)

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -1,12 +1,40 @@
 public class DoublyLinkedList<E> implements List<E>{
+    public Node<E> head;
+    public Node<E> getHead() {
+        return head;
+    }
+
+
+
+
+
+
 
     @Override
     public void add(E element) {
-
+        if (getHead() == null){
+            Node<E> head = new Node<>(element);
+        }
+        else{
+            Node<E> temp = head;
+            while (temp.getNextNode() != null){ //Work temp all the way down to the end of the node chain
+                temp = temp.getNextNode();
+            }
+            temp.setNextNode(new Node<>(element)); //Make a new node after temp to continue the chain
+        }
     }
 
     @Override
     public void add(int i, E element) throws IndexOutOfBoundsException {
+        if (i > size() - 1){
+            throw new IndexOutOfBoundsException();
+        }
+        else{
+            Node<E> temp = head;
+            for (int e = 0; e < i; e++){
+                temp = head.getNextNode(); //VISUALIZE the switching of Nexts at home with a WHITE BOARD!!!
+            }
+        }
 
     }
 
@@ -32,11 +60,24 @@ public class DoublyLinkedList<E> implements List<E>{
 
     @Override
     public int size() {
+        if (getHead() != null){
+            Node<E> temp = head;
+            int count = 0;
+            while (temp.getNextNode() != null){
+                temp = temp.getNextNode();
+                count ++;
+
+            }
+            return count;
+        }
         return 0;
     }
 
     @Override
     public boolean isEmpty() {
+        if (size() == 0){
+            return true;
+        }
         return false;
     }
 }
